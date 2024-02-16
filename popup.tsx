912251ef9f099d4f93c4441288fbc30207848001
ahnsv <ahnsv@bc.edu>
2024-02-17 00:34:13 +0900

    refactor: update popup tsx

--- popup.tsx
+++ popup.tsx
@@ -9,16 +9,11 @@ function IndexPopup() {
         padding: 16
       }}>
       <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
+        Closer makes us closer to each other
       </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+      <div>
+        <p>https://closer.so</p>
+      </div>
     </div>
   )
 }
